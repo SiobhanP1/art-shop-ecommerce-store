7ceb10b33b378cb6086e5d888c4f9342c4fd5887
SiobhanP1 <siobhanphillips11@gmail.com>
2023-12-10 12:01:33 +0000

    Create home app, url path and view

--- art_shop/settings.py
+++ art_shop/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 SITE_ID = 1
@@ -71,7 +72,10 @@ ROOT_URLCONF = 'art_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR,  'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
