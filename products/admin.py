c17ea88897307d2c6fbb815961f95e1467ecc511
SiobhanP1 <siobhanphillips11@gmail.com>
2023-12-10 22:00:11 +0000

    Create artwork view and url paths in products app

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 from .models import Artwork, Artist
 
 
-admin.site.register(Artwork)
-admin.site.register(Artist)
+class ArtworkAdmin(admin.ModelAdmin):
+    list_display = ('title', 'artist', 'description', 'price', 'main_media', 'image', 'image_url')
+    ordering = ('title',)
+
+
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'about_artist')
+    ordering = ('full_name',)
+    
+
+admin.site.register(Artwork, ArtworkAdmin)
+admin.site.register(Artist, ArtistAdmin)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Artwork, Artist
 
-# Create your views here.
+
+def all_artwork(request):
+    """A view to show all artwork including sort and search results"""
+
+    all_artwork = Artwork.objects.all()
+    context = {'all_artwork': all_artwork,}
+    return render(request, 'products/all_artwork.html', context)
