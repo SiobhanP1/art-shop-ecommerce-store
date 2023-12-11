19b4d64e616f5172ba67dc9dee17f11e59abc502
SiobhanP1 <siobhanphillips11@gmail.com>
2023-12-11 16:59:35 +0000

    Add search by keyword functionality to all_artwork view

--- products/views.py
+++ products/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
+from django.contrib import messages
+
 from .models import Artwork, Artist
 
 
@@ -6,7 +9,22 @@ def all_artwork(request):
     """A view to show all artwork including sort and search results"""
 
     all_artwork = Artwork.objects.all()
-    context = {'all_artwork': all_artwork,}
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, 'Please enter a search query.')
+                return redirect(reverse('all_artwork'))
+            queries = Q(title__icontains=query) | Q(description__icontains=query) | Q(main_media__icontains=query) 
+            all_artwork = all_artwork.filter(queries)
+
+    # all_artwork refers to query, sort or view all request results
+    context = {
+        'all_artwork': all_artwork,
+        'search_term': query,
+        }
     return render(request, 'products/all_artwork.html', context)
 
 
