85d0b0b58608d3dfe3a67482975faa9c594c653a
SiobhanP1 <siobhanphillips11@gmail.com>
2023-12-14 11:49:15 +0000

    Add basic styling to allauth templates

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def profile(request):
+    """View to display user profile"""
+    template = 'profile/profile.html'
+    context = {}
+    return render(request, template, context)
