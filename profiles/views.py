abdcaeee764486f67980208c645d574b78121eda
SiobhanP1 <siobhanphillips11@gmail.com>
2023-12-14 13:20:28 +0000

    Add save profile info to checkout_success view if user checks save_info box on form and add login_required decorator to profile function

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .forms import UserProfileForm
+from .models import UserProfile
 
 
+@login_required
 def profile(request):
     """View to display user profile"""
 
