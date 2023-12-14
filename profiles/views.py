fa0f4bcd87ee5a2747baca47d67afa8584722dcb
SiobhanP1 <siobhanphillips11@gmail.com>
2023-12-14 12:38:18 +0000

    Create user profile form in forms.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
+from .forms import UserProfileForm
+
 
 def profile(request):
     """View to display user profile"""
+
+    profile = get_object_or_404(UserProfile, user=request.user)
+    form = UserProfileForm(instance=profile)
+
+    orders = profile.orders.all()
+
     template = 'profile/profile.html'
-    context = {}
+    context = {
+        'form': form,
+        'orders': orders,
+    }
     return render(request, template, context)
