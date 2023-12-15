c4765592bd1699025468804feb5e214b710c639f
SiobhanP1 <siobhanphillips11@gmail.com>
2023-12-15 01:34:40 +0000

    Update settings so that debug turned off when in production

--- art_shop/settings.py
+++ art_shop/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,13 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-806pwi96x5upxz8jj@z^=c-fjs(@!41o0i7j*sl(y13e#x34nt'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-siobhanp1-artshopecomme-z1e63246gu9.ws-eu106.gitpod.io',
-                 '8000-siobhanp1-artshopecomme-z1e63246gu9.ws-eu107.gitpod.io',]
+                 '8000-siobhanp1-artshopecomme-z1e63246gu9.ws-eu107.gitpod.io',
+                 'art-shop-ecommerce.herokuapp.com',
+                 'localhost',]
 
 
 # Application definition
@@ -114,12 +117,17 @@ WSGI_APPLICATION = 'art_shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
