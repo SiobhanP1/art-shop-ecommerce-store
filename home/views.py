2a7045be1bd6950b92947461d46c7973e3eee351
SiobhanP1 <siobhanphillips11@gmail.com>
2024-01-23 01:40:56 +0000

    Create basic about and delivery page templates, urls and views

--- home/views.py
+++ home/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 def index(request):
     """A view to return the home page"""
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """A view to return the about page"""
+    return render(request, 'home/about.html')
+
+
+def delivery(request):
+    """A view to return the delivery page"""
+    return render(request, 'home/delivery.html')
