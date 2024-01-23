021253733b5935234c4c49414686ae44d2f2195d
SiobhanP1 <siobhanphillips11@gmail.com>
2024-01-23 21:39:18 +0000

    Set debug to false in settings.py

--- art_shop/settings.py
+++ art_shop/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-siobhanp1-artshopecomme-z1e63246gu9.ws-eu106.gitpod.io',
                  '8000-siobhanp1-artshopecomme-z1e63246gu9.ws-eu107.gitpod.io',
